54fd23a324b2d2767715feb343fe1b6eecad1a20
xiaochenMate <458339341@qq.com>
2025-12-16 11:36:28 +0800

    部署Vercel

--- types.ts
+++ types.ts
@@ -20,3 +20,18 @@ export interface PointCloudData {
   colors: Float32Array;         // Pixel colors
   sizes: Float32Array;          // Particle sizes
 }
+
+// Fix for React Three Fiber JSX elements not being recognized by TypeScript
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      points: any;
+      bufferGeometry: any;
+      bufferAttribute: any;
+      pointsMaterial: any;
+      ambientLight: any;
+      color: any;
+      fog: any;
+    }
+  }
+}
